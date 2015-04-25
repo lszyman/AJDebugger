2912cd9606580e4591f8a46bbf02e3eb03402e85
szymenek <szymenek@gmail.com>
2015-04-25 20:40:31 +0200

    added breakpoint list to gui

--- src/pl/edu/agh/toik/gui/MenuFrame.java
+++ src/pl/edu/agh/toik/gui/MenuFrame.java
@@ -13,6 +13,7 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
 
 import org.reflections.Reflections;
 import org.reflections.ReflectionsException;
@@ -25,11 +26,16 @@ import org.reflections.util.FilterBuilder;
 public class MenuFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTree tree;
-	private DefaultMutableTreeNode treeRoot;
-	private JTextField txtFldPackageSearch;
+	private final String defaultPackage = "pl";
 	
+	private JPanel contentPane;
+	private JTree treePackages;
+	private DefaultMutableTreeNode treePackagesRoot;
+	private JTextField txtFldPackageSearch;
+	private JList<String> jlistBreakpoints;
+	private DefaultListModel<String> breakpoints = new DefaultListModel<String>();
+	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -46,40 +52,62 @@ public class MenuFrame extends JFrame {
 	public MenuFrame() throws IOException {
 		setTitle("AJDebugger");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 800);
+		setBounds(100, 100, 900, 800);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		treeRoot = new DefaultMutableTreeNode("Root");
-		createPackageTree("pl");
-		tree = new JTree(treeRoot);
-		tree.setRootVisible(false);
-		tree.setBorder(new EmptyBorder(5, 5, 5, 5));
+		createLabels();
+		createButtons();
 		
-		JScrollPane scrollPane = new JScrollPane(tree);
-		scrollPane.setBounds(15, 45, 400, 600);
-		contentPane.add(scrollPane);
+		treePackagesRoot = new DefaultMutableTreeNode("Root");
+		createPackageTree(defaultPackage);
+		treePackages = new JTree(treePackagesRoot);
+		treePackages.setRootVisible(false);
+		treePackages.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		JScrollPane spTreePackages = new JScrollPane(treePackages);
+		spTreePackages.setBounds(15, 50, 400, 600);
+		contentPane.add(spTreePackages);
 		
 		txtFldPackageSearch = new JTextField();
-		txtFldPackageSearch.setText("pl");
-		txtFldPackageSearch.setBounds(150, 16, 235, 25);
+		txtFldPackageSearch.setText(defaultPackage);
+		txtFldPackageSearch.setBounds(140, 15, 235, 30);
 		contentPane.add(txtFldPackageSearch);
 		
-		JLabel lblNewLabel = new JLabel("Look in package");
-		lblNewLabel.setBounds(15, 16, 150, 20);
-		contentPane.add(lblNewLabel);
+		jlistBreakpoints = new JList<String>(breakpoints);
+		jlistBreakpoints.setBorder(new EmptyBorder(5, 5, 5, 5));
 		
-		BufferedImage buttonIcon = ImageIO.read(new File("images/search.png"));
+		JScrollPane spBreakpoints = new JScrollPane(jlistBreakpoints);
+		spBreakpoints.setBounds(465, 50, 400, 600);
+		contentPane.add(spBreakpoints);
 		
-		JButton btnLookFor = new JButton(new ImageIcon(buttonIcon));
+	}
+	
+	private void createLabels() {
+		JLabel lblLookInPackage = new JLabel("Look in package:");
+		lblLookInPackage.setBounds(15, 15, 150, 30);
+		contentPane.add(lblLookInPackage);
+		
+		JLabel lblYourBreakpoints = new JLabel("Your breakpoints:");
+		lblYourBreakpoints.setBounds(470, 15, 150, 30);
+		contentPane.add(lblYourBreakpoints);
+	}
+	
+	private void createButtons() throws IOException {
+		BufferedImage btnIconSearch = ImageIO.read(new File("images/search.png"));
+		BufferedImage btnIconForward = ImageIO.read(new File("images/forward.png"));
+		BufferedImage btnIconBack = ImageIO.read(new File("images/back.png"));
+		
+		JButton btnLookFor = new JButton(new ImageIcon(btnIconSearch));
+		btnLookFor.setBounds(383, 12, 32, 32);
 		btnLookFor.setBorder(BorderFactory.createEmptyBorder());
 		btnLookFor.setContentAreaFilled(false);
 		btnLookFor.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e) {
-				DefaultTreeModel model = (DefaultTreeModel)tree.getModel();
+				DefaultTreeModel model = (DefaultTreeModel)treePackages.getModel();
 				DefaultMutableTreeNode root = (DefaultMutableTreeNode)model.getRoot();
 				
 				root.removeAllChildren();
@@ -87,11 +115,42 @@ public class MenuFrame extends JFrame {
 				model.reload(root);
 			}
 		});
-		btnLookFor.setBounds(390, 15, 25, 25);
 		contentPane.add(btnLookFor);
+		
+		JButton btnSetBreakpoint = new JButton(new ImageIcon(btnIconForward));
+		btnSetBreakpoint.setBounds(420, 220, 40, 40);
+		btnSetBreakpoint.setContentAreaFilled(false);
+		//btnSetBreakpoint.setBorderPainted(false);
+		btnSetBreakpoint.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				TreePath treePath = treePackages.getSelectionPath();
+				if(treePath != null) {
+					int count = treePath.getPathCount();
+					String newElement = treePath.getPathComponent(count-2)+"."+treePath.getLastPathComponent();
+					if(!breakpoints.contains(newElement))
+						breakpoints.addElement(newElement);
+				}
+			}
+		});
+		contentPane.add(btnSetBreakpoint);//breakpoints
+		
+		JButton btnUnsetBreakpoint = new JButton(new ImageIcon(btnIconBack));
+		btnUnsetBreakpoint.setBounds(420, 275, 40, 40);
+		btnUnsetBreakpoint.setContentAreaFilled(false);
+		//btnUnsetBreakpoint.setBorderPainted(false);
+		btnUnsetBreakpoint.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				int index = jlistBreakpoints.getSelectedIndex();
+				if(index != -1)
+					breakpoints.remove(index);
+			}
+		});
+		contentPane.add(btnUnsetBreakpoint);
 	}
 	
-	public void createPackageTree(String packageName) {
+	private void createPackageTree(String packageName) {
 		List<String> packs = findPackageNamesStartingWith(packageName);
 		
 		for(String pack : packs) {
@@ -123,11 +182,11 @@ public class MenuFrame extends JFrame {
 					packageItem.add(classItem);
 				}
 			}
-			treeRoot.add(packageItem);
+			treePackagesRoot.add(packageItem);
 		}
 	}
 	
-	public static Set<Class<? extends Object>> getAllClasses(String packageName) {
+	private Set<Class<? extends Object>> getAllClasses(String packageName) {
 		try {
 			List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
 			classLoadersList.add(ClasspathHelper.contextClassLoader());
@@ -145,7 +204,7 @@ public class MenuFrame extends JFrame {
 		}
 	}
 	
-	public static Method[] getMethods(String className) {
+	private Method[] getMethods(String className) {
 		Class<? extends Object> aClass = null;
 		Method[] methods = null;
 		try {
@@ -158,7 +217,7 @@ public class MenuFrame extends JFrame {
 		return methods;
 	}
 	
-	public List<String> findPackageNamesStartingWith(String prefix) {
+	private List<String> findPackageNamesStartingWith(String prefix) {
 	    List<String> result = new ArrayList<String>();
 	    for(Package p : Package.getPackages()) {
 	        if (prefix == null || prefix.equals("") || p.getName().startsWith(prefix)) {
